57b336e5483022f7f73e22f359b1aa66225bfa46
Nazar Oliynyk <nazar_lw@ukr.net>
2019-07-05 14:38:40 +0300

    Initial Commit branch1

---
+++ src/testBranch1/MainBranch1.java
@@ -0,0 +1,8 @@
+package testBranch1;
+
+
+public class MainBranch1 {
+    public static void main(String[] args) {
+        System.out.println("Branch1");
+    }
+}
---
+++ src/testBranch2/MainBranch2.java
@@ -0,0 +1,8 @@
+package testBranch2;
+
+
+public class MainBranch2 {
+    public static void main(String[] args) {
+        System.out.println("Branch2");
+    }
+}
